4aa636d7d69f8571357edc71856b5d5ed4217d8b
Guest User <guest@to.isac.cnr.it>
2021-04-30 16:24:02 +0200

    beginning

---
+++ prog/IB/IB_crosscheck.py
@@ -0,0 +1,30 @@
+import numpy as np
+import xarray as xr
+import matplotlib.pyplot as plt
+
+fn1 = "/home/guest/work/michele/data/ERA5/ERA5_pIB_daily_djfm_northem_2015-2019.nc" #mio file
+fn2 = "/home/guest/work/michele/data/CLIM_CRSCHK/D12_Full_ERA5_2015_2019_DJFM.nc"
+fn3 = "/home/guest/work/michele/data/CLIM_CRSCHK/D12_Clim_ERA5_2015_2019_DJFM.nc"
+
+data1= xr.load_dataset(fn1,decode_times=False)
+data2= xr.load_dataset(fn2,decode_times=False)
+data3= xr.load_dataset(fn3,decode_times=False)
+
+pIB1= data1.pIB_boolean.values
+pIB2= data2.InstBlock.values
+
+pIB1_f= data1.pIB_frequencies.values
+pIB3_f= data3.InstBlock.values
+
+pIBdiff = data1.pIB_boolean.values - data2.InstBlock.values
+pIB_fdiff = pIB1_f - pIB3_f
+
+print(sum(sum(sum(sum(data1.pIB_boolean.values)))))
+print(sum(sum(sum(sum(data2.InstBlock.values)))))
+print(sum(sum(sum(sum(pIBdiff)))))
+
+print("\n\n")
+
+print(sum(sum(sum(pIB1_f))))
+print(sum(sum(sum(sum(pIB3_f)))))
+print(sum(sum(sum(sum(pIB_fdiff)))))
---
+++ prog/IB/pIB.py
@@ -0,0 +1,57 @@
+import numpy as np
+import xarray as xr
+import matplotlib.pyplot as plt
+
+fn = "/home/guest/work/michele/data/ERA5/ERA5_Z500_day_r144x73_500hPa_northem_1979-2019.nc" #nome del file di partenza
+fn_out = "/home/guest/work/michele/data/ERA5/ERA5_pIB_daily_northem_1979-2019.nc"
+#____CDO FILTERS____
+#dataset from ERA5 reanalysis, containing zg at 500hPa, in dec, jan, feb , march
+#from 2015 to 2019, northern emisphere. Grid dimension: 2.5°
+
+#____IMPORT DATA____
+#store zg in a xarray
+data = xr.load_dataset(fn)
+zg = data["zg"]
+print(data)
+
+#____CHECK GEOP HEIGHT____
+#ERA5 dataset uses geopotential, not height
+if zg.values[0,0,0,0] > 10000:
+	zg = zg/9.80665
+
+#____DEF punctual IB____
+def pIB(ZG):
+
+	#.values gives tuples
+	#compute GHGS/GHGN
+	GHGS = (+ ZG.loc[:,:,30:75,:].values - ZG.loc[:,:,15:60,:].values)/15.0
+	GHGN = (- ZG.loc[:,:,30:75,:].values + ZG.loc[:,:,45:90,:].values)/15.0
+
+	#look for grid points where the conditions for pIB are satisfied
+	#using where function from xarray
+	#first term is GHGN condition, second term is GHGS condition. Tuples are multiplied
+	#(no matrix product)
+	TuplepIB = xr.where(GHGN < -10.0, 1.0, 0.0) * xr.where(GHGS > 0., 1.0 , 0.0)
+
+	#define XArray using the costructor for an appropriate output
+	times = ZG.coords["time"].values
+	plev = ZG.coords["plev"].values
+	lon = ZG.coords["lon"].values
+	lat = ZG.coords["lat"].values
+	pIB = xr.DataArray(0,coords=[times,plev,lat,lon],dims = ZG.dims)
+	pIB.loc[:,:,30:75,:] = TuplepIB
+	return pIB
+
+
+#CHECK RESULTS
+pIB_b = pIB(zg) #pIB boolean
+
+#add punctual IB boolean variable to dataset
+data = data.assign(pIB_boolean = pIB_b)
+
+#add punctual IB frequencies variable to dataset
+pIB_f = sum(pIB_b)*100/pIB_b.values.shape[0]
+data = data.assign(pIB_frequencies = pIB_f)
+
+print(data)
+data.to_netcdf(fn_out)
