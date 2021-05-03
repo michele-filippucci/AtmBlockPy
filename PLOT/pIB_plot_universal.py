882959ccde0b8fd7105669b4933bc11ae0c1df12
Guest User <guest@to.isac.cnr.it>
2021-05-03 12:06:18 +0200

    reorg

--- PLOT/pIB_plot_universal.py
+++ PLOT/pIB_plot_universal.py
@@ -5,17 +5,20 @@ import numpy as np
 import xarray as xr
 
 fn = "/home/guest/work/michele/data/ERA5/ERA5_pIB_daily_northem_1979-2019.nc"
+dat_nam1 = "zg"
+dat_nam2 = "pIB_boolean"
+day = "1979-01-01T09:00:00"
+plot_title = '500-hPa ERA5 zg ' + day
 
-day = "2010-01-01T09:00:00"
 
 # Import Data
 ds = xr.open_dataset(fn)
 lats = ds.lat.data
 lons = ds.lon.data
-zg_zero = ds["zg"]
-pIB_b_zero = ds["pIB_boolean"]
-zg = zg_zero.loc[day,5e+04,:,:].values/9.80665
-pIB_b = pIB_b_zero.loc[day,5e+04,:,:].values
+dat1_zero = ds[dat_nam1]
+dat2_zero = ds[dat_nam2]
+dat1 = dat1_zero.loc[day,5e+04,:,:].values/9.80665
+dat2 = dat2_zero.loc[day,5e+04,:,:].values
 
 # Set up the projection that will be used for plotting the map
 mapcrs = ccrs.PlateCarree()
@@ -28,16 +31,16 @@ ax.set_extent([-15, 70, 30, 80], ccrs.PlateCarree())
 ax.coastlines()
 
 #plot contour
-cs_range = np.arange(4700,6100,40)
+cs_range = np.arange(4600,6100,40)
 cb_range = range(0,1)
 
-cs = ax.contourf(lons, lats,zg, cs_range,cmap="jet", transform=datacrs)
-cs_b = ax.contour(lons, lats,zg, cs_range,colors="black",linewidths = 0.1 ,transform=datacrs)
+cs = ax.contourf(lons, lats,dat1, cs_range,cmap="jet", transform=datacrs) #first ploot (float)
+cs_b = ax.contour(lons, lats,dat1, cs_range,colors="black",linewidths = 0.1 ,transform=datacrs)#second plot for contours
 plt.clabel(cs_b, colors  ="black", fontsize ="smaller", inline ="false")
-cb = ax.contour(lons,lats, pIB_b, cb_range,cmap = "flag", transform=datacrs)
+cb = ax.contour(lons,lats, dat2, cb_range,cmap = "flag", transform=datacrs)#blocking plot
 
 # Make some nice titles for the plot (one right, one left)
-plt.title('500-hPa ERA5 zg ' + day, loc='left')
+plt.title(plot_title, loc='left')
 plt.colorbar(cs, orientation='horizontal', pad=0, label = "geopotential height" , aspect=50)
 #plt.colorbar.label("% over the period")
 # Show image
