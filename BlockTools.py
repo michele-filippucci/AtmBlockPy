58bc1f56036d6c73a4ec989562ae944abaf07de5
Guest User <guest@to.isac.cnr.it>
2021-05-17 19:09:42 +0200

    changes

--- BlockTools.py
+++ BlockTools.py
@@ -1,77 +1,113 @@
 import numpy as np
 import xarray as xr
+from scipy.ndimage.measurements import label
+
+np.set_printoptions(precision=2,threshold=np.inf)
 
 def GetIndex(ds,coord="",key=""):
-    index = 0
-    for x in ds[coord].values:
-        if str(x) == key:
-            break
-        index += 1
-    return index
+  index = 0
+  for x in ds[coord].values:
+    if str(x) == key:
+      break
+    index += 1
+  return index
 
 class BlockTools(object):
-    num_of_BlockTools = 0
+  num_of_BlockTools = 0
 
-    def __init__(self):
-        BlockTools.num_of_BlockTools += 1
+  def __init__(self):
+    BlockTools.num_of_BlockTools += 1
 
-    def read(self,filename):
-        self.dataset = xr.load_dataset(filename)
+  def read(self,filename):
+    self.dataset = xr.load_dataset(filename)
 
-    """
-    This function is capable of creating an nc file identical (located in fn_out)
-    to the BlockTools.dataset with additional attributes:
-    pIB_boolean and (when freq_also == True) pIB_frequencies
-    As an alternative you can change the flag "data_return" and the function
-    will return a dataset object from the class xarray containing the same
-    additional attributes
-    """
 
-    def boolean_pIB(self,fn_out = "",data_return = False,freq_also = False):
-        if fn_out=="" and data_return==False:
-            string = "Specify the kind of output you want"
-            print(string)
-            return 0
-        #checking if dataset is right
-        try:
-            zg = self.dataset["zg"]
-            string = "data successfully received"
-        except:
-            string = "zg variable was not found.\n\ Hint: use read() to load data."
-            print(string)
-            return 0
+  """
+  This function is capable of creating an nc file identical (located in fn_out)
+  to the BlockTools.dataset with additional attributes:
+  pIB_boolean and (when freq_also == True) pIB_frequencies
+  As an alternative you can change the flag "data_return" and the function
+  will return a dataset object from the class xarray containing the same
+  additional attributes
+  """
 
-        #____CHECK GEOP HEIGHT____
-        #ERA5 dataset uses geopotential, not height
-        if zg.values[0,0,0,0] > 10000:
-        	zg = zg/9.80665
+  def boolean_pIB(self,fn_out = "",data_return = False,freq_also = False):
+    if fn_out=="" and data_return==False:
+      string = "Specify the kind of output you want"
+      print(string)
+      return 0
+    #checking if dataset is right
+    try:
+      zg = self.dataset["zg"]
+      string = "data successfully received"
+    except:
+      string = "zg variable was not found.\n\ Hint: use read() to load data."
+      print(string)
+      return 0
 
-        #.values gives tuples
-        #compute GHGS/GHGN
-        GHGS = (+ zg.loc[:,:,30:75,:].values - zg.loc[:,:,15:60,:].values)/15.0
-        GHGN = (- zg.loc[:,:,30:75,:].values + zg.loc[:,:,45:90,:].values)/15.0
+    #____CHECK GEOP HEIGHT____
+    #ERA5 dataset uses geopotential, not height
+    if zg.values[0,0,0,0] > 10000:
+    	zg = zg/9.80665
 
-        #look for grid points where the conditions for pIB are satisfied
-        #using where function from xarray
-        #first term is GHGN condition, second term is GHGS condition. Tuples are multiplied
-        #(no matrix product)
-        TuplepIB = xr.where(GHGN < -10.0, 1.0, 0.0) * xr.where(GHGS > 0., 1.0 , 0.0)
+    #.values gives tuples
+    #compute GHGS/GHGN
+    GHGS = (+ zg.loc[:,:,30:75,:].values - zg.loc[:,:,15:60,:].values)/15.0
+    GHGN = (- zg.loc[:,:,30:75,:].values + zg.loc[:,:,45:90,:].values)/15.0
 
-        #define XArray using the costructor for an appropriate output
-        times = zg.coords["time"].values
-        plev = zg.coords["plev"].values
-        lon = zg.coords["lon"].values
-        lat = zg.coords["lat"].values
-        pIB = xr.DataArray(0,coords=[times,plev,lat,lon],dims = zg.dims)
-        pIB.loc[:,:,30:75,:] = TuplepIB
-        self.dataset = self.dataset.assign(pIB_boolean = pIB)
-        if freq_also == True:
-            pIB_f = sum(pIB)*100/pIB.values.shape[0]
-            self.dataset = self.dataset.assign(pIB_frequencies = pIB_f)
-        if data_return == False:
-            self.dataset.to_netcdf(fn_out)
-        if data_return == True:
-            print(string)
-            return self.dataset
-        else:
-            return 0
+    #look for grid points where the conditions for pIB are satisfied
+    #using where function from xarray
+    #first term is GHGN condition, second term is GHGS condition. Tuples are multiplied
+    #(no matrix product)
+    TuplepIB = xr.where(GHGN < -10.0, 1.0, 0.0) * xr.where(GHGS > 0., 1.0 , 0.0)
+
+    #define XArray using the costructor for an appropriate output
+    times = zg.coords["time"].values
+    plev = zg.coords["plev"].values
+    lon = zg.coords["lon"].values
+    lat = zg.coords["lat"].values
+    pIB = xr.DataArray(0,coords=[times,plev,lat,lon],dims = zg.dims)
+    pIB.loc[:,:,30:75,:] = TuplepIB
+    self.dataset = self.dataset.assign(pIB_boolean = pIB)
+    if freq_also == True:
+      pIB_f = sum(pIB)*100/pIB.values.shape[0]
+      self.dataset = self.dataset.assign(pIB_frequencies = pIB_f)
+    if data_return == False:
+      self.dataset.to_netcdf(fn_out)
+    if data_return == True:
+      print(string)
+      return self.dataset
+    else:
+      return 0
+
+  """
+  Contour Tracking
+  """
+  def ContourTracking(self,fn_out = "",var_name = "pIB_boolean",data_return = False):
+    if fn_out=="" and data_return==False:
+      string = "Specify the kind of output you want"
+      print(string)
+      return 0
+    try:
+      pIB_boolean = self.dataset[var_name]
+    except:
+      print("Error Code 1: dataset not valid. The variable " + var_name + " cannot be found")
+      return 1
+    arr = pIB_boolean.values[:,0,:,:]
+    structure = np.ones((3,3,3))
+    #print(np.shape(arr),np.shape(structure))
+    arr,ncomponents=label(arr,structure)
+    times = pIB_boolean.coords["time"].values
+    plev = pIB_boolean.coords["plev"].values
+    lon = pIB_boolean.coords["lon"].values
+    lat = pIB_boolean.coords["lat"].values
+    pIB_tracked = xr.DataArray(0,coords=[times,plev,lat,lon],dims = pIB_boolean.dims)
+    pIB_tracked[:,:,:,:] = 0
+    pIB_tracked[:,0,:,:] = arr
+    self.dataset = self.dataset.assign(pIB_tracked = pIB_tracked)
+    if data_return == False:
+      self.dataset.to_netcdf(fn_out)
+    if data_return == True:
+      return self.dataset
+    else:
+      return 0
--- pIB_tryclass.py
+++ pIB_tryclass.py
@@ -1,5 +1,8 @@
 import numpy as np
 import xarray as xr
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import cartopy.util as cutil
 
 #my own class
 from BlockTools import BlockTools
@@ -17,19 +20,44 @@ print(z)
 """
 
 fn = "/home/guest/work/michele/data/ERA5/Z500/ERA5_Z500_day_djfm_r144x73_500hPa_northem_1979-2019.nc"
-fn2 = "/home/guest/work/michele/data/ERA5/T500/ERA5_T500_day_djfm_northem_r144x73_1979-2019.nc"
+#fn2 = "/home/guest/work/michele/data/ERA5/T500/ERA5_T500_day_djfm_northem_r144x73_1979-2019.nc"
+fn2 = "/home/guest/work/michele/data/ERA5/precipitation/ERA5_precipitation_day_djfm_northem_r144x73_1979-2019.nc"
 
 fn_out = "/home/guest/work/michele/data/ERA5/processed/ERA5_pIB_daily_djfm_northem_1979-2019.nc"
+fn_out2 = "/home/guest/work/michele/data/ERA5/processed/ERA5_pIB_tracked_daily_djfm_northem_1979-2019.nc"
+img_out = "/home/guest/work/michele/prog/plots/Tracking.png"
 
-img_out = "/home/guest/work/michele/prog/plots/zg500composite_tempanomaly.png"
+#try contour tracking
+contourpIB = BlockTools()
+contourpIB.read(fn_out)
+#ds = contourpIB.ContourTracking(data_return = True)
+#print(ds["pIB_tracked"])
+contourpIB.ContourTracking(fn_out2)
+
+plot = BlockPlots("tracking 1980-01-01T09:00:00 - 1980-01-15T09:00")
+plot.read_main(fn_out2)
+#plot.read_additional_ds(fn2)
+print(plot.PlotTracking(output = img_out,days=["1979-01-01T09:00:00.000000000","1979-01-16T09:00:00.000000000"]))
 """
 #try BlockTools
 pIB = BlockTools()
 pIB.read(fn)
 print(pIB.boolean_pIB(fn_out,freq_also = True))
 """
-plot = BlockPlots("ZG500 composite vs temp anomaly")
+
+"""
+#try composite temperature
+plot = BlockPlots("ZG500 composite vs temp anomaly, 55 N, 10 E")
 plot.read_main(fn_out)
 plot.read_additional_ds(fn2)
 #try composite
 print(plot.PlotCompositeZ500(output = img_out,additional = "ta",point_coords=[55.0,10.0]))
+"""
+"""
+#try composite precipitation
+plot = BlockPlots("ZG500 composite vs prec anomaly, 55 N, 10 E")
+plot.read_main(fn_out)
+plot.read_additional_ds(fn2)
+print(plot.PlotCompositeZ500(output = img_out, additional ="tp",mapcrs = ccrs.PlateCarree(),point_coords=[55.0,10.0],colorbar_label = "Precipitation (mm)",extent=[-70,70,20,70]))
+"""
+
